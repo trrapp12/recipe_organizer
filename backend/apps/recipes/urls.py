802e87aa957bcaf76352efc9040a11ac07c2961b
Trrapp12 <trrapp@gmail.com>
2015-04-28 11:16:13 -0600

    changed the gitignore to allow the media to pass through for a git push

--- backend/apps/recipes/urls.py
+++ backend/apps/recipes/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.conf import settings
 from views import RecipeList, RecipeDetail, AddRecipe
 
-urlpatterns = patterns ('',
+urlpatterns = patterns('',
     url('^recipes/$', RecipeList.as_view(), name='recipe-list'),
     url('^recipes/(?P<pk>[0-9]+)/$', RecipeDetail.as_view(), name='recipe-detail'),
     url('^add-recipe/$', AddRecipe.as_view(), name='add-recipe'),
